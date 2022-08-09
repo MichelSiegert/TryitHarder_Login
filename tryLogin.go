bad58e8454879174cb5f84819b833ee7ac469980
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2022-08-09 13:00:23 +0200

    Basic Program for API calls.

---
+++ tryLogin.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var i = 0
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func increment(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK,
+		gin.H{
+			"message": i,
+		})
+	i++
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/test", increment)
+	router.Run("localhost:8080")
+}
