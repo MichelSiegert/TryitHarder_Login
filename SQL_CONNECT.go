26de5bb9dba63136d2185a9264de2ab4ed028b2d
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2022-08-25 18:59:47 +0200

    adjusting it so it actually works in the cloud now.

--- SQL_CONNECT.go
+++ SQL_CONNECT.go
@@ -1,46 +1,22 @@
 package main
 
 import (
-	"cloud.google.com/go/cloudsqlconn"
-	"context"
 	"database/sql"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
-	"net"
 )
 
 func connectDB() (*sql.DB, error) {
 
-	// Note: Saving credentials in environment variables is convenient, but not
-	// secure - consider a more secure solution such as
-	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-	// keep secrets safe.
-	var (
-		dbUser                 = "tryituserbase"                          // e.g. 'my-db-user'
-		dbPwd                  = "voJ<kCK4~)T.-H,k"                       // e.g. 'my-db-password'
-		dbName                 = "TryItHarder"                            // e.g. 'my-database'
-		instanceConnectionName = "tryitharder:europe-west3:tryituserbase" // e.g. 'project:region:instance'
-		usePrivate             = ""
-	)
-
-	d, err := cloudsqlconn.NewDialer(context.Background())
+	user := "tryituserbase"
+	password := "voJ<kCK4~)T.-H,k"
+	hostname := "34.159.221.111"
+	port := "3306"
+	dbname := "TryItHarder"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, hostname, port, dbname)
+	dbPool, err := sql.Open("mysql", dsn)
 	if err != nil {
-		return nil, fmt.Errorf("cloudsqlconn.NewDialer: %v", err)
-	}
-	mysql.RegisterDialContext("cloudsqlconn",
-		func(ctx context.Context, addr string) (net.Conn, error) {
-			if usePrivate != "" {
-				return d.Dial(ctx, instanceConnectionName, cloudsqlconn.WithPrivateIP())
-			}
-			return d.Dial(ctx, instanceConnectionName)
-		})
-
-	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true",
-		dbUser, dbPwd, dbName)
-
-	dbPool, err := sql.Open("mysql", dbURI)
-	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %v", err)
+		fmt.Println("sql.open: " + err.Error())
+		return nil, err
 	}
 	return dbPool, nil
 }
