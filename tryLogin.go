394f4abd6c0bca35a08536c222abfcb0e2d98d52
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2022-08-25 20:52:58 +0200

    making the failed login information more helpful since "DIDNT WORK LOL" was bad. (#19)

--- tryLogin.go
+++ tryLogin.go
@@ -8,17 +8,13 @@ import (
 
 func tryLogin(user userData, db *sql.DB, c echo.Context) error {
 	var response Response
-	token := checkUser(user, db)
+	data, token := checkUser(user, db)
 	if token == "-1" {
-		response.Message = "failed to login! \n"
-		response.Data = "there are several possible issues: database issues, or the data is just wrong"
+		response.Message = token
+		response.Data = data
 		response.httpstatus = http.StatusOK
 		response.Mail = user.Email
-	} else {
-		response.Data = token
-		response.Mail = user.Email
-		response.Message = " successfully loggeed in!"
-		response.httpstatus = http.StatusOK
 	}
+
 	return c.JSON(response.httpstatus, response)
 }
